67cc741604819a0582219c3ef1b22b9bcb26dd38
1234e <1234eggs@gmail.com>
2023-09-13 12:00:06 +0800

    初始化 Spring Boot project

---
+++ Donweb/src/main/java/com/donweb/DonwebApplication.java
@@ -0,0 +1,13 @@
+package com.donweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonwebApplication.class, args);
+    }
+
+}
